2132f3001d29b9bc93456a9bcf5c17c041f336d2
leocadpin <leopoldocadavid@gmail.com>
2023-02-10 12:02:58 +0100

    nueva img + splitted edges

--- detection.py
+++ detection.py
@@ -13,9 +13,25 @@ from pyparsing import RecursiveGrammarException
 from torch import sqrt_
 
 # path = '/home/leo/Escritorio/Uware/1_imagenes/underwater_id/images/img1.webp'
-path = '/home/leo/Escritorio/Uware/1_imagenes/underwater_id/images/img2.png'
+# path = '/home/leo/Escritorio/Uware/1_imagenes/underwater_id/images/img2.png'
+path = '/home/leo/Escritorio/Uware/1_imagenes/underwater_id/images/img3.png'
 img = cv.imread(path)
 
+# print('Original Dimensions : ',img.shape)
+ 
+# scale_percent = 40 # percent of original size
+# width = int(img.shape[1] * scale_percent / 100)
+# height = int(img.shape[0] * scale_percent / 100)
+# dim = (width, height)
+  
+# # resize image
+# resized = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+ 
+# print('Resized Dimensions : ',resized.shape)
+ 
+# # cv.imshow("Resized image", resized)
+
+# img= resized
 
 def morphology_filters(edges):
         # Forma del filtro
@@ -33,31 +49,40 @@ def morphology_filters(edges):
     edges_erode = cv.erode(edges_erode, element2)
     edges_erode = cv.erode(edges_erode, element2)
     edges_erode = cv.erode(edges_erode, element3)
+    edges_erode = cv.erode(edges_erode, element3)
+    edges_erode = cv.erode(edges_erode, element3)
+    edges_erode = cv.erode(edges_erode, element3)
+    edges_erode = cv.erode(edges_erode, element3)
+    edges_erode = cv.erode(edges_erode, element3)
     return edges_erode
 #-------------------------------------  PRE-PROCESAMIENTO   ----------------------
-# img2 = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-img_lab = cv.cvtColor(img, cv.COLOR_RGB2LAB)
-rows, columns, channel  = img_lab.shape
-l, a, b = cv.split(img_lab)
+def preprocessing(img):
+    # img2 = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    img_lab = cv.cvtColor(img, cv.COLOR_RGB2LAB)
+    rows, columns, channel  = img_lab.shape
+    l, a, b = cv.split(img_lab)
 
-l_blur = cv.medianBlur(l, 5)
+    l_blur = cv.medianBlur(l, 5)
 
-for row in range(rows):
-    for column in range(columns):
-        l_in=  img_lab[row, column, 0]
-        l_out = (1.5*l_in) - (0.5*l_blur[row, column]) 
-        img_lab[row, column, 0] = l_out
-        
-result = cv.cvtColor(img_lab, cv.COLOR_LAB2RGB)
-result_gr = cv.cvtColor(img_lab, cv.COLOR_RGB2GRAY)
-# create a CLAHE object (Arguments are optional).
-clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    for row in range(rows):
+        for column in range(columns):
+            l_in=  img_lab[row, column, 0]
+            l_out = (1.5*l_in) - (0.5*l_blur[row, column]) 
+            img_lab[row, column, 0] = l_out
+            
+    result = cv.cvtColor(img_lab, cv.COLOR_LAB2RGB)
+    result_gr = cv.cvtColor(img_lab, cv.COLOR_RGB2GRAY)
+    # create a CLAHE object (Arguments are optional).
+    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 
-img_lab[...,0] = clahe.apply(img_lab[...,0])
+    img_lab[...,0] = clahe.apply(img_lab[...,0])
 
 
 
-result_prep = cv.cvtColor(img_lab, cv.COLOR_LAB2RGB)
+    result_prep = cv.cvtColor(img_lab, cv.COLOR_LAB2RGB)
+    return result_prep
+
+result_prep = preprocessing(img)
 
 #--------------Clusterización -----------------------------------------#
 
@@ -70,81 +95,125 @@ img_rgb = cv.pyrMeanShiftFiltering(img_rgb, 20, 20)
 # img_rgb = result_prep
 img_hsv = cv.cvtColor(img_rgb, cv.COLOR_RGB2HSV)
 
-# Conversion a escala de grises 
-r_img , g_img, b_img = split(img_rgb)
+
+def splitted_sobels(img_rgb):
+    # Conversion a escala de grises 
+    r_img , g_img, b_img = split(img_rgb)
 
 
-# imshow('r_channel', r_img)
-# imshow('g_channel', g_img)
-# imshow('b_channel', b_img)
+    # imshow('r_channel', r_img)
+    # imshow('g_channel', g_img)
+    # imshow('b_channel', b_img)
 
-# r_img = img_rgb.copy()
-# r_img[:,:,0] =0
+    # r_img = img_rgb.copy()
+    # r_img[:,:,0] =0
 
-# g_img = img_rgb.copy()
-# # g_img[:,:, 1] =0
-# g_img[:,:, 1] =0
+    # g_img = img_rgb.copy()
+    # # g_img[:,:, 1] =0
+    # g_img[:,:, 1] =0
 
-# b_img = img_rgb.copy()
-# b_img[:,:,2] =0
-
-
-# imshow('r_img_channel', r_img)
-# imshow('g_img_channel', g_img)
-# imshow('b_img_channel', b_img)
+    # b_img = img_rgb.copy()
+    # b_img[:,:,2] =0
 
 
-# #Gradientes
-ddepth = cv.CV_16S
+    # imshow('r_img_channel', r_img)
+    # imshow('g_img_channel', g_img)
+    # imshow('b_img_channel', b_img)
 
-r_grad_x = cv.Sobel(r_img, ddepth, 1, 0, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
-r_grad_y = cv.Sobel(r_img, ddepth, 0, 1, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+
+    # #Gradientes
+    ddepth = cv.CV_16S
+
+    r_grad_x = cv.Sobel(r_img, ddepth, 1, 0, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+    r_grad_y = cv.Sobel(r_img, ddepth, 0, 1, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+        
+    r_abs_grad_x = cv.convertScaleAbs(r_grad_x)
+    r_abs_grad_y = cv.convertScaleAbs(r_grad_y)
+
+    g_grad_x = cv.Sobel(g_img, ddepth, 1, 0, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+    g_grad_y = cv.Sobel(g_img, ddepth, 0, 1, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+        
+    g_abs_grad_x = cv.convertScaleAbs(g_grad_x)
+    g_abs_grad_y = cv.convertScaleAbs(g_grad_y)
+
+    b_grad_x = cv.Sobel(b_img, ddepth, 1, 0, ksize=9, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+    b_grad_y = cv.Sobel(b_img, ddepth, 0, 1, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+        
+    b_abs_grad_x = cv.convertScaleAbs(b_grad_x)
+    b_abs_grad_y = cv.convertScaleAbs(b_grad_y)  
+
+
+    #TODO: IMPLEMENTAR CALCULO DE SOBEL CON RAIZ CUADRADA   
+    # sqr_grad = ((grad_x ** 2) + (grad_y ** 2))**0,5
+
+    r_abs_grad = cv.addWeighted(r_abs_grad_x, 0.5, r_abs_grad_y, 0.5, 0)
+    g_abs_grad = cv.addWeighted(g_abs_grad_x, 0.5, g_abs_grad_y, 0.5, 0)
+    b_abs_grad = cv.addWeighted(b_abs_grad_x, 0.5, b_abs_grad_y, 0.5, 0)
+
+    # imshow('r_img_channel', r_abs_grad)
+    # imshow('g_img_channel', g_abs_grad)
+    # imshow('b_img_channel', b_abs_grad)
+    # # print(np.shape(features))
+    return r_abs_grad, g_abs_grad, b_abs_grad
+
+
+r_abs_grad, g_abs_grad, b_abs_grad = splitted_sobels(img_rgb)
+
+def multi_edges(r_abs_grad, g_abs_grad, b_abs_grad):
+    aperture_size = 3
+    histeresys_min_thres = 100
+    histeresys_max_thres = 180
+    r_edges = cv.Canny(r_abs_grad, histeresys_min_thres, histeresys_max_thres, None, aperture_size)
+    g_edges = cv.Canny(g_abs_grad, histeresys_min_thres, histeresys_max_thres , None, aperture_size)
+    b_edges = cv.Canny(b_abs_grad, histeresys_min_thres, histeresys_max_thres, None, aperture_size)
+    r_edges_erode = morphology_filters(r_edges)
+    g_edges_erode = morphology_filters(g_edges)
+    b_edges_erode = morphology_filters(b_edges)
+    return r_edges_erode, g_edges_erode, b_edges_erode, r_edges, g_edges, b_edges
+
+r_edges_erode, g_edges_erode, b_edges_erode, r_edges, g_edges, b_edges = multi_edges(r_abs_grad,
+                                                          g_abs_grad,
+                                                          b_abs_grad)
     
-r_abs_grad_x = cv.convertScaleAbs(r_grad_x)
-r_abs_grad_y = cv.convertScaleAbs(r_grad_y)
-
-g_grad_x = cv.Sobel(g_img, ddepth, 1, 0, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
-g_grad_y = cv.Sobel(g_img, ddepth, 0, 1, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
-    
-g_abs_grad_x = cv.convertScaleAbs(g_grad_x)
-g_abs_grad_y = cv.convertScaleAbs(g_grad_y)
-
-b_grad_x = cv.Sobel(b_img, ddepth, 1, 0, ksize=9, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
-b_grad_y = cv.Sobel(b_img, ddepth, 0, 1, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
-    
-b_abs_grad_x = cv.convertScaleAbs(b_grad_x)
-b_abs_grad_y = cv.convertScaleAbs(b_grad_y)  
-
-
-#TODO: IMPLEMENTAR CALCULO DE SOBEL CON RAIZ CUADRADA   
-# sqr_grad = ((grad_x ** 2) + (grad_y ** 2))**0,5
-
-r_abs_grad = cv.addWeighted(r_abs_grad_x, 0.5, r_abs_grad_y, 0.5, 0)
-g_abs_grad = cv.addWeighted(g_abs_grad_x, 0.5, g_abs_grad_y, 0.5, 0)
-b_abs_grad = cv.addWeighted(b_abs_grad_x, 0.5, b_abs_grad_y, 0.5, 0)
-
-# imshow('r_img_channel', r_abs_grad)
-# imshow('g_img_channel', g_abs_grad)
-# imshow('b_img_channel', b_abs_grad)
-# # print(np.shape(features))
-
-edges = cv.Canny(b_abs_grad, 50, 200, None, 3)
-
-
-edges_erode = morphology_filters(edges)
 # Ejecutamos Hough
-lines = cv.HoughLinesP(edges_erode, 1, np.pi/180, 10, 30, 60, 7)
+min_long = int(columns*0.25)
+min_gap = int(columns*0.025)
+r_lines = cv.HoughLinesP(r_edges_erode, 1, np.pi/180, 10, 30, min_long, min_gap)
+g_lines = cv.HoughLinesP(g_edges_erode, 1, np.pi/180, 10, 30, min_long, min_gap)
+b_lines = cv.HoughLinesP(b_edges_erode, 1, np.pi/180, 10, 30, min_long, min_gap)
 
 # Dibujamos las líneas resultantes sobre una copia de la imagen original
 dst = img.copy()
-if lines is not None:
-    for i in range(0, len(lines)):
-        l = lines[i][0]
-        cv.line(dst, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv.LINE_AA)
+dst2 = img.copy()
+dst3 = img.copy()
 
-imshow('bordes', edges)
-imshow('Lineas', dst)
-imshow('Lineas_erode', edges_erode)
+if r_lines is not None:
+    for i in range(0, len(r_lines)):
+        l = r_lines[i][0]
+        cv.line(dst, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv.LINE_AA)
+if g_lines is not None:
+    for i in range(0, len(g_lines)):
+        l = g_lines[i][0]
+        cv.line(dst2, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv.LINE_AA)
+if b_lines is not None:
+    for i in range(0, len(b_lines)):
+        l = b_lines[i][0]
+        cv.line(dst3, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv.LINE_AA)
+
+
+imshow('gradientes de red channel', r_abs_grad)
+imshow('gradientes de green channel', g_abs_grad)
+imshow('gradientes de blue channel', b_abs_grad)
+imshow('bordes_erode r', r_edges_erode)
+imshow('bordes_erode g', g_edges_erode)
+imshow('bordes_erode b', b_edges_erode)
+imshow('bordes r', r_edges)
+imshow('bordes g', g_edges)
+imshow('bordes b', b_edges)
+imshow('Lineas r', dst)
+imshow('Lineas g', dst2)
+imshow('Lineas b', dst3)
+# imshow('Lineas_erode', edges_erode)
 #CREAMOS VECTOR DE FEATURES
 h, s, v = cv.split(img_hsv)
 
