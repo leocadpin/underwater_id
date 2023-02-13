82fc70953633bf444b54063937633b781b350402
leocadpin <leopoldocadavid@gmail.com>
2023-02-13 14:36:01 +0100

    bash scrip added

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python3 detection.py -i  img1.png
+python3 detection.py -i  img2.png
+python3 detection.py -i  img3.png
+python3 detection.py -i  img4.png
+python3 detection.py -i  img5.png
+python3 detection.py -i  img6.png
+python3 detection.py -i  img7.png
+python3 detection.py -i  img8.png
